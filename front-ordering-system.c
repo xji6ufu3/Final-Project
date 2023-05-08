70f77e6b136eee324d75206070f8c803c16908bb
max2124 <max93.01.11@gmail.com>
2023-05-08 20:53:40 +0800

    update front.c
    fixed count_price().
    check number in number.txt before start input, if the number is not 0, initialize number.txt or exit the program.
    when DEBUG, the output add meal name.

--- front-ordering-system.c
+++ front-ordering-system.c
@@ -11,38 +11,60 @@ typedef struct list_t{
 } list_t; // list_t store the order information
 enum state_t{NOTHING,CONTINUE,BREAK};
 enum state_t check_meal_code(char str[]);
+double price[LEN] = {1,2,3,4,5};
 double count_price(char str[]);
 char *meal_name[LEN] = {"salads","hamberger","soup","tea","ice cream"};
-double price[LEN] = {1.0,2.0,3.0,4.0,5.0};
 void free_list(list_t *ptr);
+int cmp(const void *a, const void *b){
+    return (*(char*)a - *(char*)b);
+}
 int main(){
-    int count, price = 0; // count store the sum of order
+    int count; // count store the sum of order
     char str[LEN+1], record[36];
     enum state_t state;
     list_t *list = NULL, *ptr, *prior; // the beginning of the list
     FILE *frecord = fopen("recording.txt","a+");
-    FILE *fnum = fopen("number.txt","w+");
+    FILE *fnum = fopen("number.txt","r");
     fscanf(fnum,"%d",&count);
-    printf("count: %d.\n",count);
+    while(count){
+        printf("the number in number.txt is not 0.\n"
+        "scan command below to continue.\n"
+        "1: initialize the number.txt\n"
+        "2: exit the program.\n"
+        "command: ");
+        scanf("%d",&state);
+        if(state == CONTINUE){
+            fclose(fnum);
+            fnum = fopen("number.txt","w");
+            fprintf(fnum,"%d",0);
+            count = 0;
+        }
+        else if(state == BREAK) return 0;
+        else printf("\nyour input is not 1 or 2.\n"
+        "please try again.\n\n");
+    }
+    setbuf(stdin,NULL); // clear input buffer 
     while(1){
         printf("input the meal code: ");
         fgets(str,LEN,stdin);
         str[strlen(str)-1] = '\0';
+        if(!strlen(str)) {
+            printf("\nempty meal codes!\n"
+            "please try again.\n\n");
+            continue;
+        }
         state = check_meal_code(str);
         if(state == BREAK) break;
         else if(state == CONTINUE) continue;
         // sort the meal code and count the total price
         // store the meal codes to the list
+        qsort(str,strlen(str),sizeof(char),cmp);
         if(count == 0){ // if it's first order
             list = malloc(sizeof(list_t)+strlen(str)+1);
             ptr = list;
             list->prior = list->next = NULL;
         }
         else{ // not first order
-            if(list == NULL){
-                printf("the number in number.txt is not 0.\n");
-                break;
-            }
             ptr->next = malloc(sizeof(list_t)+strlen(str)+1);
             prior = ptr;
             ptr = ptr->next;
@@ -53,6 +75,8 @@ int main(){
         strcpy(ptr->meal,str);
         ptr->price = count_price(str);
     }
+    fclose(fnum);
+    fnum = fopen("number.txt","w");
     fprintf(fnum,"%d",count);
     fclose(fnum);
     fclose(frecord);
@@ -62,9 +86,12 @@ int main(){
     for(int i = 0; i < count; i++){
         printf(
         "num: %d\n"
-        "meal: %s\n"
-        "price: %d\n"
-        "----------------\n",ptr->number,ptr->meal,ptr->price);
+        "meal code: %s\n",ptr->number,ptr->meal);
+        printf("meal name: %s",meal_name[ptr->meal[0]-'a']);
+        for(int j = 1; j < strlen(ptr->meal); j++) printf(" %s",meal_name[ptr->meal[j]-'a']);
+        printf(
+        "\nprice: %.2lf\n"
+        "----------------\n",ptr->price);
         ptr = ptr->next;
     }
     #endif
@@ -102,9 +129,10 @@ enum state_t check_meal_code(char str[]){
 }
 double count_price(char str[]){
     double sum = 0;
-    int len = strlen(str);
+    int len = strlen(str), j;
     for(int i = 0; i < len; i++){
-        sum += (price[(str[i]-'a')]);
+        j = str[i] - 'a';
+        sum += price[j];
     }
     // printf("sum: %d.\n",sum);
     return sum;
