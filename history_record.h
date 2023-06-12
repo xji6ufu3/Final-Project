3ea315b90d41fb0df4147872e507fefd0687ce93
weikikiii <132288830+weikikiii@users.noreply.github.com>
2023-06-12 15:29:51 +0800

    Update history_record.h
    新增counting();

--- history_record.h
+++ history_record.h
@@ -6,5 +6,6 @@ int record(float a, int b, char word[10]); // 錢, 幾道菜, 結局代碼(A or 
 int history_show(int num);                 //  玩過幾次
 int restart(int num);
 int his_search(int choice, int num);
+int counting();
 
 #endif
