419296a7b3ac4c02fc298f2336e39ba17f855913
weikikiii <132288830+weikikiii@users.noreply.github.com>
2023-06-12 17:14:47 +0800

    Update history_record.h
    add values earn_initialization();

--- history_record.h
+++ history_record.h
@@ -1,11 +1,19 @@
 #ifndef history_record_h
 #define history_record_h
 
+typedef struct
+{
+    int his_earn;
+    int his_dishes;
+    int choice;
+} values;
+
 int num_func();
 int record(float a, int b, char word[10]); // 錢, 幾道菜, 結局代碼(A or B or C)
 int history_show(int num);                 //  玩過幾次
 int restart(int num);
 int his_search(int choice, int num);
 int counting();
+values earn_initialization();
 
 #endif
